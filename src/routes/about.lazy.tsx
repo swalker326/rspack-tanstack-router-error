282cd1ff75e0671df7fed5ec43d4581fe7694610
Shane <shane@swalker.dev>
2024-11-04 05:25:41 -0600

    first commit

---
+++ src/routes/about.lazy.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/about")({
+  component: RouteComponent
+});
+
+function RouteComponent() {
+  return "Hello /about!";
+}
